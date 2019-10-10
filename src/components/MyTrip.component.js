e34a564062b876e8ef478a82e5d5e320cc8910bf
SiGnaLz <e3.141x@gmail.com>
2019-10-10 13:54:56 -0400

    Fixed DatePicker Styling

--- src/components/MyTrip.component.js
+++ src/components/MyTrip.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
 
 export default class MyTrip extends Component {
